d7bb18a828302e6a177a6a41e9e0b69f331313e0
Denis Carriere <carriere.denis@gmail.com>
2026-01-11 17:48:32 -0500

    fix sell

--- clickhouse/schema.3.view.user_position.sql
+++ clickhouse/schema.3.view.user_position.sql
@@ -21,7 +21,7 @@ SELECT
     sum(buy_cost) AS buy_cost,
     sum(sell_revenue) AS sell_revenue,
     -- Realized PNL (sell_revenue - proportional buy_cost) approximation --
-    sum(sell_revenue) - sum(buy_cost) AS realized_pnl,
+    sum(state_user_position.sell_revenue) - sum(state_user_position.buy_cost) AS realized_pnl,
     -- Scaled amounts (USDC has 6 decimals, so divide by 10^6) --
     toFloat64(sum(state_user_position.buy_amount)) / 1000000.0 AS scaled_buy_amount,
     toFloat64(sum(state_user_position.sell_amount)) / 1000000.0 AS scaled_sell_amount,
@@ -64,7 +64,7 @@ SELECT
     -- Cost basis tracking --
     sum(buy_cost) AS buy_cost,
     sum(sell_revenue) AS sell_revenue,
-    sum(sell_revenue) - sum(buy_cost) AS realized_pnl,
+    sum(state_user_position.sell_revenue) - sum(state_user_position.buy_cost) AS realized_pnl,
     -- Scaled amounts --
     toFloat64(sum(state_user_position.buy_amount)) / 1000000.0 AS scaled_buy_amount,
     toFloat64(sum(state_user_position.sell_amount)) / 1000000.0 AS scaled_sell_amount,
@@ -103,9 +103,9 @@ SELECT
     sum(sell_amount) AS sell_amount,
     sum(net_amount) AS net_amount,
     -- Cost basis tracking --
-    sum(buy_cost) AS buy_cost,
-    sum(sell_revenue) AS sell_revenue,
-    sum(sell_revenue) - sum(buy_cost) AS realized_pnl,
+    sum(state_user_position.buy_cost) AS buy_cost,
+    sum(state_user_position.sell_revenue) AS sell_revenue,
+    sum(state_user_position.sell_revenue) - sum(state_user_position.buy_cost) AS realized_pnl,
     -- Scaled amounts --
     toFloat64(sum(state_user_position.buy_amount)) / 1000000.0 AS scaled_buy_amount,
     toFloat64(sum(state_user_position.sell_amount)) / 1000000.0 AS scaled_sell_amount,
